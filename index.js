ceddf8fe10f784af80be9692602a1e804fc6c10a
Albert Pichler <albertpichler@gmx.net>
2022-05-06 13:41:01 +0200

    Fix routing check for request outside public + log cleanup

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ import path from 'node:path';
 import url from 'node:url';
 
 function handleReadFile(pathname, response, filetypes, ext) {
-  console.log(`i got ${pathname} and type ${ext}`)
+  console.log(`Request to ${pathname} with type ${ext}`);
   fs.readFile(pathname, (err, contents) => {
     if (err) {
-      console.log(err)
+      // console.log(err);
       if (err.code === 'EISDIR') {
         pathname += '/index.html';
-        ext = ".html"
-        console.log(pathname)
-        console.log("call again")
-        console.log("type " + ext)
-        return handleReadFile(pathname, response, filetypes, ext)
+        ext = '.html';
+        console.log(
+          `Caught invalid file request to dir. Requesting again with ${pathname} and type ${ext}`,
+        );
+        return handleReadFile(pathname, response, filetypes, ext);
+      } else if (err.code === 'ENOENT') {
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.write('404 - Requested resource was not found');
+        response.end();
+        return;
       }
-      console.log("set err type")
+      console.log(`Respond with error for request to ${pathname}`);
       response.writeHead(500, { 'Content-Type': 'text/plain' });
       response.write(err + '\n');
       response.end();
@@ -25,21 +30,28 @@ function handleReadFile(pathname, response, filetypes, ext) {
     response.writeHead(200, {
       'Content-type': filetypes[ext] || 'text/plain',
     });
-    //response.write(contents, 'binary');
     response.end(contents);
   });
-
 }
 
 const server = http.createServer(function (request, response) {
   console.log(request.url, request.method);
-  // parse URL
+
   const parsedUrl = url.parse(request.url);
-  // extract URL path
   let pathname = `.${parsedUrl.pathname}`;
-  // based on the URL path, extract the file extension. e.g. .js, .doc, ...
-  const ext = path.parse(pathname).ext;
-  // maps file extension to MIME typere
+  let ext = path.parse(pathname).ext;
+
+  if (pathname === './' && ext === '') {
+    pathname = 'public/index.html';
+    ext = '.html';
+  } else if (pathname === './index.html') {
+    pathname = 'public/index.html';
+  } else if (!pathname.startsWith('./public')) {
+    response.writeHead(403, { 'content-type': 'text/plain' });
+    response.write('403 - Forbidden');
+    response.end();
+    return;
+  }
 
   const filetypes = {
     '.ico': 'image/x-icon',
@@ -56,33 +68,8 @@ const server = http.createServer(function (request, response) {
     '.doc': 'application/msword',
   };
 
-  // 1. check request method
   if (request.method === 'GET') {
-    //if (fs.stat(pathname).isDirectory()) {
-    //  pathname += '/index' + ext;
-    //}
-
     handleReadFile(pathname, response, filetypes, ext);
-    /* fs.readFile(pathname, (err, contents) => {
-      if (err) {
-        console.log(err)
-        if (err.code === 'EISDIR') {
-          pathname += '/index.html';
-          // return func call?
-        }
-        response.writeHead(500, { 'Content-Type': 'text/plain' });
-        response.write(err + '\n');
-        response.end();
-        return;
-      }
-      response.writeHead(200, {
-        'Content-type': filetypes[ext] || 'text/plain',
-      });
-      //response.write(contents, 'binary');
-      response.end(contents);
-    }); */
-
-    // handle invalid request methods
   } else {
     response.writeHead(405, { 'content-type': 'text/plain' });
   }
